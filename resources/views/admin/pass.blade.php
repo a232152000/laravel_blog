de7938c3ed073c4d661aacc848c961d131e8264f
a232152000 <a232152000@gmail.com>
2017-06-30 12:08:26 +0800

    後台管理員密碼修改和Validation的驗證(問題修正)

--- resources/views/admin/pass.blade.php
+++ resources/views/admin/pass.blade.php
@@ -13,17 +13,17 @@
 	<div class="result_title">
 			<h3>修改密碼</h3>
 
-					@if(count($errors)>0)
-						<div class="mark">
-							@if(is_object($errors))
-									@foreach($errors -> all() as $error)
-											<p>{{$errors}}</p>
-									@endforeach
-							@else
-									<p>{{$errors}}</p>
-							@endif
-						</div>
-					@endif
+				@if(count($errors)>0)
+					<div class="mark">
+						@if(is_object($errors))
+							@foreach($errors -> all() as $error)
+								<p>{{$error}}</p>
+							@endforeach
+						@else
+							<p>{{$errors}}</p>
+						@endif
+					</div>
+				@endif
 
 	</div>
 </div>
