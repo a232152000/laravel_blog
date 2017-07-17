cd3d2cbed54f1314114b59210bfbe7f3d5197257
a232152000 <a232152000@gmail.com>
2017-07-17 20:10:24 +0800

    修改添加文章和文章列表的子頁面

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -23,34 +23,33 @@
 <!--左侧导航 开始-->
 <div class="menu_box">
 	<ul>
-					<li>
-						<h3><i class="fa fa-fw fa-clipboard"></i>常用操作</h3>
-							<ul class="sub_menu">
-								<li><a href="{{url('admin/category/create')}}" target="main"><i class="fa fa-fw fa-plus-square"></i>添加分頁</a></li>
-								<li><a href="{{url('admin/category')}}" target="main"><i class="fa fa-fw fa-list-ul"></i>分頁列表</a></li>
-
-								<li><a href="{{url('admin/article/create')}}"><i class="fa fa-plus"></i>添加文章</a></li>
-								<li><a href="{{url('admin/article')}}"><i class="fa fa-recycle"></i>文章列表</a></li>
-							</ul>
-					</li>
-					<li>
-						<h3><i class="fa fa-fw fa-cog"></i>系統設置</h3>
-							<ul class="sub_menu" style="display: block">
-								<li><a href="{{url('admin/links')}}" target="main"><i class="fa fa-fw fa-cubes"></i>友情鏈結</a></li>
-								<li><a href="{{url('admin/navs')}}" target="main"><i class="fa fa-navicon"></i>自定義導航</a></li>
-								<li><a href="#" target="main"><i class="fa fa-fw fa-database"></i>備份還原</a></li>
-							</ul>
-					</li>
-					<li>
-						<h3><i class="fa fa-fw fa-thumb-tack"></i>工具導航</h3>
-							<ul class="sub_menu">
-									<li><a href="http://www.yeahzan.com/fa/facss.html" target="main"><i class="fa fa-fw fa-font"></i>圖標調用</a></li>
-									<li><a href="http://hemin.cn/jq/cheatsheet.html" target="main"><i class="fa fa-fw fa-chain"></i>Jquery手册</a></li>
-									<li><a href="http://tool.c7sky.com/webcolor/" target="main"><i class="fa fa-fw fa-tachometer"></i>配色板</a></li>
-									<li><a href="element.html" target="main"><i class="fa fa-fw fa-tags"></i>其他組件</a></li>
-							</ul>
-					</li>
-			</ul>
+			<li>
+				<h3><i class="fa fa-fw fa-clipboard"></i>常用操作</h3>
+					<ul class="sub_menu">
+						<li><a href="{{url('admin/category/create')}}" target="main"><i class="fa fa-fw fa-plus-square"></i>添加分頁</a></li>
+						<li><a href="{{url('admin/category')}}" target="main"><i class="fa fa-fw fa-list-ul"></i>分頁列表</a></li>
+						<li><a href="{{url('admin/article/create')}}" target="main"><i class="fa fa-plus"></i>添加文章</a></li>
+						<li><a href="{{url('admin/article')}}" target="main"><i class="fa fa-recycle"></i>文章列表</a></li>
+					</ul>
+			</li>
+			<li>
+				<h3><i class="fa fa-fw fa-cog"></i>系統設置</h3>
+					<ul class="sub_menu" style="display: block">
+						<li><a href="{{url('admin/links')}}" target="main"><i class="fa fa-fw fa-cubes"></i>友情鏈結</a></li>
+						<li><a href="{{url('admin/navs')}}" target="main"><i class="fa fa-navicon"></i>自定義導航</a></li>
+						<li><a href="#" target="main"><i class="fa fa-fw fa-database"></i>備份還原</a></li>
+					</ul>
+			</li>
+			<li>
+				<h3><i class="fa fa-fw fa-thumb-tack"></i>工具導航</h3>
+					<ul class="sub_menu">
+							<li><a href="http://www.yeahzan.com/fa/facss.html" target="main"><i class="fa fa-fw fa-font"></i>圖標調用</a></li>
+							<li><a href="http://hemin.cn/jq/cheatsheet.html" target="main"><i class="fa fa-fw fa-chain"></i>Jquery手册</a></li>
+							<li><a href="http://tool.c7sky.com/webcolor/" target="main"><i class="fa fa-fw fa-tachometer"></i>配色板</a></li>
+							<li><a href="element.html" target="main"><i class="fa fa-fw fa-tags"></i>其他組件</a></li>
+					</ul>
+			</li>
+	</ul>
 </div>
 <!--左侧导航 结束-->
 
